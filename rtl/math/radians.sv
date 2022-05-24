4a584a752cf306f4c912b68cd40cc248eb5ee843
YanWQ-monad <YanWQmonad@gmail.com>
2022-05-24 17:38:00 +0800

    refactor(math): optimize degree to radians

--- rtl/math/radians.sv
+++ rtl/math/radians.sv
@@ -9,14 +9,8 @@ module radians (
     output      logic signed [`FLOAT_BITS-1:0] out
 );
 
-    logic signed [`FLOAT_BITS-1:0] in2;
-    logic signed [`FLOAT_DOUBLE_BITS-1:0] theta;
-
-    always_comb begin
-        in2 = { in, { `FLOAT_DCM_BITS{1'b0} } };
-        theta = (in2 * (`PI) * (`INV_180)) >>> (`FLOAT_DCM_BITS * 2);
-        out = theta[`FLOAT_BITS-1:0];
-    end
+    always_comb
+        out = in * (`DEGREE_TO_RADIAN);
 
 endmodule
 
