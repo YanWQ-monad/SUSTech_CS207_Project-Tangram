c7c08ed73725bfc8a97001a0bf1c8863541462fb
YanWQ-monad <YanWQmonad@gmail.com>
2022-05-25 00:31:37 +0800

    refactor(input): improve robustness of debounce

--- rtl/input/input_debounce.sv
+++ rtl/input/input_debounce.sv
@@ -5,25 +5,23 @@
 module input_debounce (
     input  wire logic clk,   // clock
     input  wire logic in,    // signal input
-    output      logic out,   // signal output (debounced)
+    output      logic out = 0,   // signal output (debounced)
     output      logic ondn,  // on down (one tick)
     output      logic onup   // on up (one tick)
 );
-
-    initial out = 0;
 
     // sync with clock and combat metastability
     logic sync_0, sync_1;
     always_ff @(posedge clk) sync_0 <= in;
     always_ff @(posedge clk) sync_1 <= sync_0;
 
-    logic [17:0] cnt;  // 2^18 = 2.6 ms counter at 100 MHz
+    logic [17:0] cnt = 0;  // 2^18 = 6.55 ms counter at 40 MHz
     logic idle, max;
     always_comb begin
         idle = (out == sync_1);
         max  = &cnt;
-        ondn = !idle & max & !out;
-        onup = !idle & max & out;
+        ondn = (~idle) & max & (~out);
+        onup = (~idle) & max & out;
     end
 
     always_ff @(posedge clk) begin
@@ -31,7 +29,8 @@ module input_debounce (
             cnt <= 0;
         end else begin
             cnt <= cnt + 1;
-            if (max) out <= !out;
+            if (max)
+                out <= ~out;
         end
     end
 endmodule
