b361324ed5f2aa17b5d84cc411e39e2bb7d1fc67
YanWQ-monad <YanWQmonad@gmail.com>
2022-05-27 21:18:16 +0800

    test(render): fix test_render_shape

--- tests/test_render_shape/test_render_shape.py
+++ tests/test_render_shape/test_render_shape.py
@@ -1,26 +1,49 @@
+import math
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles, RisingEdge, Timer
 
+from utils import fixed_float, unfixed_float
+
 
 def set_dut(dut, ty, x0, y0, size, angle):
     dut.ty.value = ty
-    dut.x0.value = x0
-    dut.y0.value = y0
     dut.size.value = size
-    dut.angle.value = angle
+
+    sin = math.sin(math.radians(angle))
+    cos = math.cos(math.radians(angle))
+    x0 = (-x0) * cos - (-y0) * sin
+    y0 = (-x0) * sin + (-y0) * cos
+
+    dut.sin.value = fixed_float(sin)
+    dut.cos.value = fixed_float(cos)
+    dut.ix.value = fixed_float(x0)
+    dut.iy.value = fixed_float(y0)
 
 
 async def get_coordinate(dut, x, y):
-    dut.x.value = x
-    dut.y.value = y
-    await Timer(2, units='ns')
+    # reset position
+    dut.newframe.value = 1
+    await RisingEdge(dut.clk)
+    dut.newframe.value = 0
+
+    # set Y
+    dut.newline.value = 1
+    await ClockCycles(dut.clk, y)
+    dut.newline.value = 0
+
+    # set X
+    await ClockCycles(dut.clk, x + 1)
+
     return dut.out.value.integer
 
 
 @cocotb.test()
 async def render_shape_test(dut):
     """Test render shape"""
+
+    clock = cocotb.start_soon(Clock(dut.clk, 10, 'ns').start())
 
     set_dut(dut, 0, 5, 5, 5, 0)
     assert await get_coordinate(dut, 4, 4) == 0
